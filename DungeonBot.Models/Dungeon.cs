0b73fd7d3625575b7f77b52c39e45e113b244b6e
James Schafer <james@jschafer.net>
2020-09-24 09:21:09 -0400

    Initial implementation of the ListAndDetailView and Dungeons Component

---
+++ DungeonBot.Models/Dungeon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DungeonBot.Models
+{
+    public class Dungeon
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Level { get; set; }
+
+        public IEnumerable<Encounter> Encounters { get; set; }
+
+        //public string ThumbnailImage { get; set; }
+
+        public int AvailableDungeonBots { get; set; }
+
+        //public IEnumerable<DungeonBot> DungeonBots { get; set; }
+
+        //public IEnumerable<Attempt> AttemptHistory { get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ DungeonBot.Models/Encounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DungeonBot.Models
+{
+    public class Encounter
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Level { get; set; }
+
+        //public string ThumbnailImage { get; set; }
+
+        public IEnumerable<string> Counters { get; set; }
+    }
+}
