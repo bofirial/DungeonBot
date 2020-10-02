dfbe0b92f2cc780ca12aebffdc6dcd9ecc899c06
James Schafer <james@jschafer.net>
2020-10-01 23:29:41 -0400

    Implement a Modal for DungeonResults

---
+++ Client/wwwroot/scripts/launchModal.js
@@ -0,0 +1,6 @@
+window.launchModal = (modalId) => {
+    const modal = new bootstrap.Modal(document.getElementById(modalId));
+    modal.show();
+};
+export {};
+//# sourceMappingURL=launchModal.js.map
---
+++ Client/wwwroot/scripts/launchModal.ts
@@ -0,0 +1,14 @@
+﻿export { }
+
+declare const bootstrap: any;
+declare global {
+    interface Window {
+        launchModal: any;
+    }
+}
+
+window.launchModal = (modalId) => {
+    const modal = new bootstrap.Modal(document.getElementById(modalId));
+
+    modal.show();
+};
