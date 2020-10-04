3c97008352cc92860479854fb02b703198fba532
James Schafer <james@jschafer.net>
2020-10-04 10:58:01 -0400

    Add logic to update ListAndDetailView current item when the list is changed.  Convert the DungeonDetail DungeonResultModal logic to match.

--- Client/Components/ListAndDetailView.razor.cs
+++ Client/Components/ListAndDetailView.razor.cs
@@ -7,7 +7,10 @@ namespace DungeonBot.Client.Components
     public partial class ListAndDetailView<TListItem> where TListItem : class
     {
         [Parameter]
-        public IEnumerable<TListItem> List { get; set; } = Array.Empty<TListItem>();
+        public IEnumerable<TListItem> List { get; set; }
+
+        [Parameter]
+        public Func<TListItem, string>? KeyFunction { get; set; }
 
         [Parameter]
         public RenderFragment<TListItem>? ListItemView { get; set; }
@@ -15,8 +18,34 @@ namespace DungeonBot.Client.Components
         [Parameter]
         public RenderFragment<TListItem>? DetailView { get; set; }
 
+        private string CurrentListItemKey { get; set; } = string.Empty;
+
         private TListItem? CurrentListItem { get; set; }
 
-        private void SetActiveListItem(TListItem listItem) => CurrentListItem = listItem;
+        private void SetActiveListItem(TListItem listItem)
+        {
+            if (KeyFunction != null)
+            {
+                CurrentListItemKey = KeyFunction(listItem);
+            }
+
+            CurrentListItem = listItem;
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            if (KeyFunction != null && !string.IsNullOrEmpty(CurrentListItemKey) && CurrentListItem != null)
+            {
+                foreach (var listItem in List)
+                {
+                    if (CurrentListItemKey == KeyFunction(listItem))
+                    {
+                        CurrentListItem = listItem;
+                    }
+                }
+            }
+        }
     }
 }
