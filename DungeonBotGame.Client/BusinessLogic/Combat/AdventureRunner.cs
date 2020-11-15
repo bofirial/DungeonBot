cd3aea70534d772ad6c358a222cdf383e8d70391
James Schafer <james@jschafer.net>
2020-11-14 23:38:34 -0500

    Update AdventureRunner to build DungeonBot if it has not been built yet.

--- DungeonBotGame.Client/BusinessLogic/Combat/AdventureRunner.cs
+++ DungeonBotGame.Client/BusinessLogic/Combat/AdventureRunner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DungeonBotGame.Client.BusinessLogic.Compilation;
 using DungeonBotGame.Client.Store.Adventures;
 using DungeonBotGame.Models.ViewModels;
 
@@ -15,16 +16,25 @@ namespace DungeonBotGame.Client.BusinessLogic.Combat
     {
         private readonly IDungeonBotFactory _dungeonBotFactory;
         private readonly IEncounterRunner _encounterRunner;
+        private readonly IActionModuleContextBuilder _actionModuleContextBuilder;
 
-        public AdventureRunner(IDungeonBotFactory dungeonBotFactory, IEncounterRunner encounterRunner)
+        public AdventureRunner(IDungeonBotFactory dungeonBotFactory, IEncounterRunner encounterRunner, IActionModuleContextBuilder actionModuleContextBuilder)
         {
             _dungeonBotFactory = dungeonBotFactory;
             _encounterRunner = encounterRunner;
+            _actionModuleContextBuilder = actionModuleContextBuilder;
         }
 
         public async Task<AdventureResultViewModel> RunAdventureAsync(RunAdventureAction runAdventureAction)
         {
-            var dungeonBot = _dungeonBotFactory.CreateCombatDungeonBot(runAdventureAction.DungeonBot);
+            var dungeonBotViewModel = runAdventureAction.DungeonBot;
+
+            if (dungeonBotViewModel.ActionModuleContext == null)
+            {
+                dungeonBotViewModel = await _actionModuleContextBuilder.BuildActionModuleContextAsync(dungeonBotViewModel);
+            }
+
+            var dungeonBot = _dungeonBotFactory.CreateCombatDungeonBot(dungeonBotViewModel);
 
             var encounterResults = new List<EncounterResultViewModel>();
 
