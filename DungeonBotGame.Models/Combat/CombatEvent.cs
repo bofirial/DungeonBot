25f02b7ab94c6021ad736f61bb343473023f1d28
James Schafer <james@jschafer.net>
2020-11-18 20:31:13 -0500

    Clean up CombatEvent models

--- DungeonBotGame.Models/Combat/CombatEvent.cs
+++ DungeonBotGame.Models/Combat/CombatEvent.cs
@@ -2,12 +2,5 @@
 {
     public record CombatEvent(int CombatTime, CharacterBase Character, CombatEventType CombatEventType);
 
-
     public record CombatEvent<TEventData>(int CombatTime, CharacterBase Character, CombatEventType CombatEventType, TEventData EventData) : CombatEvent(CombatTime, Character, CombatEventType);
-
-    public enum CombatEventType
-    {
-        CharacterAction = 1,
-        CooldownReset
-    }
 }
