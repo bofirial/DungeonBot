9a224a9972a5e5c7af9f10ce8d3b6e8519b559f4
James Schafer <james@jschafer.net>
2020-11-24 15:59:12 -0500

    Fix warning

--- DungeonBotGame.Tests/TestMetadataReferenceProvider.cs
+++ DungeonBotGame.Tests/TestMetadataReferenceProvider.cs
@@ -13,63 +13,9 @@ namespace DungeonBotGame.Tests
 {
     public class TestMetadataReferenceProvider : IMetadataReferenceProvider
     {
-        public async Task<List<MetadataReference>> LoadReferencesAsync()
+        public Task<List<MetadataReference>> LoadReferencesAsync()
         {
-            //var metadataReferences = new List<MetadataReference>();
-
-            //var targetReferences = new string[]
-            //{
-            //    "DungeonBotGame.dll",
-            //    "DungeonBotGame.Models.dll",
-            //    "mscorlib.dll",
-            //    "netstandard.dll",
-            //    "Newtonsoft.Json.dll",
-            //    "System.Buffers.dll",
-            //    "System.Collections.Concurrent.dll",
-            //    "System.Collections.dll",
-            //    "System.Collections.Immutable.dll",
-            //    "System.Collections.NonGeneric.dll",
-            //    "System.Collections.Specialized.dll",
-            //    "System.Console.dll",
-            //    "System.Core.dll",
-            //    "System.dll",
-            //    "System.Linq.dll",
-            //    "System.Linq.Expressions.dll",
-            //    "System.Linq.Parallel.dll",
-            //    "System.Linq.Queryable.dll",
-            //    "System.Memory.dll",
-            //    "System.Net.dll",
-            //    "System.Net.Http.dll",
-            //    "System.Numerics.dll",
-            //    "System.Numerics.Vectors.dll",
-            //    "System.ObjectModel.dll",
-            //    "System.Private.CoreLib.dll",
-            //    "System.Private.DataContractSerialization.dll",
-            //    "System.Private.Runtime.InteropServices.JavaScript.dll",
-            //    "System.Private.Uri.dll",
-            //    "System.Private.Xml.dll",
-            //    "System.Private.Xml.Linq.dll",
-            //    "System.Reflection.DispatchProxy.dll",
-            //    "System.Reflection.dll",
-            //    "System.Reflection.Emit.dll",
-            //    "System.Reflection.Emit.ILGeneration.dll",
-            //    "System.Reflection.Emit.Lightweight.dll",
-            //    "System.Reflection.Extensions.dll",
-            //    "System.Reflection.Metadata.dll",
-            //    "System.Reflection.Primitives.dll",
-            //    "System.Reflection.TypeExtensions.dll",
-            //    "System.Runtime.dll",
-            //    "System.Runtime.Extensions.dll",
-            //    "System.Threading.Tasks.Extensions.dll",
-            //    "System.ValueTuple.dll",
-            //};
-
-            //foreach (var reference in targetReferences)
-            //{
-
-            //}
-
-            return new List<MetadataReference> {
+            return Task.FromResult(new List<MetadataReference> {
                 MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
                 MetadataReference.CreateFromFile(typeof(Console).Assembly.Location),
                 MetadataReference.CreateFromFile(typeof(ActionModuleEntrypointAttribute).Assembly.Location),
@@ -78,7 +24,7 @@ namespace DungeonBotGame.Tests
                 MetadataReference.CreateFromFile(typeof(HttpClient).Assembly.Location),
                 MetadataReference.CreateFromFile(typeof(Expression).Assembly.Location),
                 MetadataReference.CreateFromFile(Assembly.Load(GetSystemRuntimeAssemblyName()).Location)
-            };
+            });
         }
 
         private static string GetSystemRuntimeAssemblyName()
