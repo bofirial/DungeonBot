7777250ab73002bdce5b862232b15c8d6b06633f
James Schafer <james@jschafer.net>
2020-11-29 19:42:53 -0500

    Implement Minimal DungeonBot Selector for the Adventure Detail Screen

---
+++ DungeonBotGame.Client/BusinessLogic/ClassDetailProvider.cs
@@ -0,0 +1,24 @@
+﻿using DungeonBotGame.Client.ErrorHandling;
+
+namespace DungeonBotGame.Client.BusinessLogic
+{
+    public interface IClassDetailProvider
+    {
+        string GetClassName(DungeonBotClass dungeonBotClass);
+    }
+
+    public class ClassDetailProvider : IClassDetailProvider
+    {
+        public string GetClassName(DungeonBotClass dungeonBotClass)
+        {
+            return dungeonBotClass switch
+            {
+                DungeonBotClass.WarriorBot => "Warrior Bot",
+                DungeonBotClass.ArmorBot => "Armor Bot",
+                DungeonBotClass.SorcererBot => "Sorcerer Bot",
+                DungeonBotClass.MysticRepairBot => "Mystic Repair Bot",
+                _ => throw new UnknownDungeonBotClassException(dungeonBotClass),
+            };
+        }
+    }
+}
