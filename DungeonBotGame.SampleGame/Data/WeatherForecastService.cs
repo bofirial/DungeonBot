f652d82fdd90ce3730602e672ca889d186e78ae4
James Schafer <james@jschafer.net>
2022-04-02 19:57:09 -0400

    Initial project structure

---
+++ DungeonBotGame.SampleGame/Data/WeatherForecastService.cs
@@ -0,0 +1,19 @@
+﻿namespace DungeonBotGame.SampleGame.Data;
+
+public class WeatherForecastService
+{
+    private static readonly string[] _summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+    {
+        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = _summaries[Random.Shared.Next(_summaries.Length)]
+        }).ToArray());
+    }
+}
