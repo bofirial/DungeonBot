2b7909bdfd9d74a2234b6668587d9dc8902afb6c
James Schafer <james@jschafer.net>
2022-04-03 16:23:19 -0400

    Move blazor from SampleGame to Blazor RCL

--- DungeonBotGame.SampleGame/Program.cs
+++ DungeonBotGame.SampleGame/Program.cs
@@ -1,18 +1,14 @@
-﻿using DungeonBotGame.SampleGame.Data;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
