c0043404922e95bcad6824ad1498d55e894d3755
James Schafer <james@jschafer.net>
2022-04-06 21:39:50 -0400

    Add Source Generator

---
+++ DungeonBot.Foundation/DungeonBotAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace DungeonBotGame.Foundation;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class DungeonBotAttribute : Attribute
+{
+    public DungeonBotAttribute(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
---
+++ DungeonBot.SourceGenerators/GenerateSourceCodePropertyPartialClassSyntaxReceiver.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DungeonBotGame.SourceGenerators;
+public class GenerateDungeonBotPartialClassSyntaxReceiver : ISyntaxReceiver
+{
+    public List<(ClassDeclarationSyntax Class, AttributeSyntax Attribute)> ClassesToAugment { get; } = new List<(ClassDeclarationSyntax Class, AttributeSyntax Attribute)>();
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is AttributeSyntax attributeSyntax &&
+            syntaxNode?.Parent?.Parent != null &&
+            syntaxNode?.Parent?.Parent is ClassDeclarationSyntax classDeclarationSyntax)
+        {
+            ClassesToAugment.Add((classDeclarationSyntax, attributeSyntax));
+        }
+    }
+}
